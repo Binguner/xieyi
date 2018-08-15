f8e62a86086633823bba93c92f5eeec4c86d1fc6
Binguner <zhengzuoyu0414@gmail.com>
2018-08-15 21:54:21 +0800

    Add stautsBar util

--- app/src/main/java/com/binguner/xieyi/MainActivity.kt
+++ app/src/main/java/com/binguner/xieyi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.binguner.xieyi
 
 import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
@@ -10,7 +11,13 @@ import org.jetbrains.anko.constraint.layout.constraintLayout
 import org.jetbrains.anko.constraint.layout.ConstraintSetBuilder.Side.*
 import android.support.constraint.ConstraintSet.PARENT_ID
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.ContextCompat.getColor
+import android.text.Layout
+import android.view.ViewManager
+import android.view.WindowManager
+import com.binguner.xieyi.utils.StatusBarUtil
 import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.ankoView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         MainActivityUI().setContentView(this)
+        setTransparentStatusbar();
+    }
+
+    private fun setTransparentStatusbar() {
+        StatusBarUtil.setStatusBarColor(this,R.color.colorWhite)
+        StatusBarUtil.setStatusBarTextBalck(this)
     }
 
 
@@ -25,26 +38,67 @@ class MainActivity : AppCompatActivity() {
 
 class MainActivityUI: AnkoComponent<MainActivity>{
     val id_toolbar = View.generateViewId()
+    val id_shadow_line = View.generateViewId()
+    val id_btn1 = View.generateViewId()
+    val id_btn2 = View.generateViewId()
+    val id_btn3 = View.generateViewId()
+
+
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         constraintLayout(){
-            backgroundColor = ContextCompat.getColor(ctx,R.color.colorNormalBack)
-            //id = id_toolbar
+            backgroundColor = getColor(ctx,R.color.colorNormalBack)
+
             var mtoolbar = toolbar{
                 id = id_toolbar
                 backgroundColor = Color.WHITE
             }.lparams(width = matchParent)
 
+            include<View>(R.layout.shadow_line){
+                id = id_shadow_line
+                fitsSystemWindows = true
+            }.lparams(width = matchParent, height = wrapContent){
+                topToBottom = id_toolbar
+                fitsSystemWindows = true
+            }
 
+            val btn1 = imageView(R.drawable.ic_crop_square_black_24dp){
+                id = id_btn1
+            }.lparams(width = dip(30)){
+                padding = dip(10)
+            }
+            val btn2 = imageView(R.drawable.ic_add_black_24dp){
+                id = id_btn2
+            }.lparams(width = dip(30)){
+                padding = dip(10)
 
+            }
+            val btn3 = imageView(R.drawable.ic_person_black_24dp){
+                id = id_btn3
+            }.lparams(width = dip(30)){
+                padding = dip(10)
+            }
 
 
             applyConstraintSet {
+                connect(
+                        START of btn1 to START of PARENT_ID,
+                        END of btn1 to START of btn2,
+                        START of btn2 to END of btn1,
+                        END of btn2 to START of btn3,
+                        START of btn3 to END of btn2,
+                        END of btn3 to END of PARENT_ID,
+                        BOTTOM of btn1 to BOTTOM of PARENT_ID,
+                        BOTTOM of btn2 to BOTTOM of PARENT_ID,
+                        BOTTOM of btn3 to BOTTOM of PARENT_ID
+                )
                 mtoolbar{
                     connect(
                             TOP to TOP of PARENT_ID
                     )
                 }
+
             }
+
         }
 
     }
--- app/src/main/java/com/binguner/xieyi/custom_view/ImageTextView.kt
+++ app/src/main/java/com/binguner/xieyi/custom_view/ImageTextView.kt
@@ -4,14 +4,17 @@ import android.app.ActionBar
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
+import android.view.ViewManager
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.binguner.xieyi.R
+import org.jetbrains.anko.*
 import org.jetbrains.anko.backgroundColor
+import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.dip
 import org.jetbrains.anko.textColor
 
-class ImageTextView(context: Context): TextView(context){
+/*class ImageTextView(context: Context): TextView(context){
     var normalDrawblePic: Drawable ?= null
     var selectedDrawblePic: Drawable ?= null
 
@@ -38,4 +41,44 @@ class ImageTextView(context: Context): TextView(context){
             }
         }
     }
+
 }
+
+public inline fun ViewManager.imageTextView() = imageTextView()
+public inline fun ViewManager.imageTextView(init: ImageTextView.() -> Unit) = ankoView({ ImageTextView(it) }, init)*/
+
+class imageTextView(context: Context) : TextView(context) {
+    var normalDrawable: Drawable? = null
+    var selectedDrawable: Drawable? = null
+
+    init {
+        var layoutParams = LinearLayout.LayoutParams(dip(50),
+                LinearLayout.LayoutParams.MATCH_PARENT, 1f)
+        layoutParams.weight = 1f
+        this.layoutParams = layoutParams
+    }
+
+    override fun setSelected(selected: Boolean) {
+        super.setSelected(selected)
+
+        if (selected) {
+            this.backgroundColor = ContextCompat.getColor(context, R.color.colorLightGrey)
+            this.textColor = ContextCompat.getColor(context, R.color.colorBlack)
+
+            if (selectedDrawable != null) {
+                this.setCompoundDrawablesWithIntrinsicBounds(null, selectedDrawable, null, null)
+            }
+        } else {
+            this.backgroundColor = ContextCompat.getColor(context, android.R.color.transparent)
+            this.textColor = ContextCompat.getColor(context, R.color.colorTextGray)
+            if (normalDrawable != null) {
+                this.setCompoundDrawablesWithIntrinsicBounds(null, normalDrawable, null, null)
+            }
+        }
+    }
+
+   /* public inline fun ViewManager.imageTextView() = imageTextView {}
+    public inline fun ViewManager.imageTextView(init: imageTextView.() -> Unit) = ankoView({ imageTextView(it) }, init)
+*/
+}
+
